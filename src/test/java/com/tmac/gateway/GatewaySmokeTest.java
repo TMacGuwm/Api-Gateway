ba557c6590948b6aa527f314372c1a69654384c1
TMac1 <6252289+TMacGuwm@users.noreply.github.com>
2026-07-20 23:30:14 +0800

    test: add gateway smoke test skeleton

---
+++ src/test/java/com/tmac/gateway/GatewaySmokeTest.java
@@ -0,0 +1,11 @@
+package com.tmac.gateway;
+
+import org.junit.jupiter.api.Test;
+
+class GatewaySmokeTest {
+
+    @Test
+    void contextSmokeTest() {
+        // integration test placeholder
+    }
+}
